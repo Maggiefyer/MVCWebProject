b5fc2073447b47863d7796de596969ac36cd53ab
Bright NZ <109766140@qq.com>
2023-03-19 17:45:46 +1300

    Add project files.

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,39 @@
+﻿using MVCWebProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace MVCWebProject.Controllers
+{   
+  
+    public class AboutController : Controller
+    {
+
+        private static List<ClientViewModel> clients = new List<ClientViewModel>();
+
+        public IActionResult Index()
+        {
+           
+           return View(clients);
+        }
+
+        public IActionResult Contact()
+        {
+
+            var ClientInfo = new ClientViewModel();
+            return View(ClientInfo);//binding the data
+        }
+
+        public IActionResult CreatClients(ClientViewModel clientviewmodel)
+        {
+            clients.Add(clientviewmodel);
+            return RedirectToAction(nameof(Index));
+        }
+       
+        public IActionResult Services()
+        {
+            return View();
+        }
+
+       
+    }
+}
---
+++ Models/ClientViewModel.cs
@@ -0,0 +1,16 @@
+﻿
+namespace MVCWebProject.Models
+{
+    public class ClientViewModel
+    {
+        public int Id { get; set; }
+        public string FName { get; set; }
+        public string LName { get; set; }        
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string BusinessDescription { get; set; }
+        public string Message { get; set; }
+
+
+    }
+}
